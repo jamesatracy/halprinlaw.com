63f28be40d7b7f10f726a5c2dbe8da224cd5b90e
James <james.a.tracy@gmail.com>
2013-09-29 13:41:00 -0700

    CSS update

--- new/views/partials/masthead.php
+++ new/views/partials/masthead.php
@@ -5,7 +5,7 @@
 		</div>
 		<div class="col-md-4">
 			<img id="phone" src="<?php $this->url("/images/old-phone.jpg"); ?>" alt="Free Phone Consultation" />
-			<h2>Free Phone Consultation</h2>
+			<h2 class="free-phone-consultation">Free Phone Consultation</h2>
 			<p>For more information on your unique situation, please call us for a <strong>free consultation</strong> at <a href="tel:+12153896913">215-389-6913</a> or <a href="tel:+1866864257746">1-866-86-HALPRIN</a>. HalprinLaw attorneys value each and every client.</p>
 		</div>
 	</div>
